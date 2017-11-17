1a046e0f321fd6e79c840aa1304157fbc4724960
Juan David Garzon Alzate <juan.alzate@yuxiglobal.com>
2017-11-17 10:42:14 -0500

    Adicion de las Pruebas de BackEnd

---
+++ ProyectoClinica.Tests/Controllers/EnfermedadesTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoClinica;
+using ProyectoClinica.Controllers;
+
+namespace ProyectoClinica.Tests.Controllers
+{
+    [TestClass]
+    public class EnfermedadesTest
+    {
+
+        EnfermedadesController controller = new EnfermedadesController();
+        [TestMethod]
+        public void TestEnfermedadesCreate()
+        {
+           
+            Enfermedades enfermedad = new Enfermedades();
+            enfermedad.nombre = "enfermedadPrueba";
+            enfermedad.descripcion = "enfermedadPruebaDescripcion";
+            Assert.IsNotNull(controller.Create(enfermedad));
+           
+        }
+
+        [TestMethod]
+        public void TestEnfermedadesDelete()
+        {
+            Assert.IsNotNull(controller.DeleteConfirmed(1004));
+        }
+
+        [TestMethod]
+        public void TestEnfermedadesFind()
+        {
+           Assert.AreNotEqual("System.Web.Mvc.HttpNotFoundResult", controller.Details(1).ToString());
+        }
+
+        [TestMethod]
+        public void TestEnfermedadesEdit()
+        {
+
+            Enfermedades enfermedad = new Enfermedades();
+            enfermedad.nombre = "enfermedadPruebaEDITADA";
+            enfermedad.descripcion = "enfermedadPruebaDescripcionEDITADA";
+            enfermedad.idEnfermedad = 1005;
+            Assert.IsNotNull(controller.Edit(enfermedad));
+
+        }
+    }
+}
---
+++ ProyectoClinica.Tests/Controllers/MedicosTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoClinica.Controllers;
+
+namespace ProyectoClinica.Tests.Controllers
+{
+    [TestClass]
+    public class MedicosTest
+    {
+
+        MedicosController controller = new MedicosController();
+        [TestMethod]
+        public void TestMedicosCreate()
+        {
+
+            Medicos medico = new Medicos();
+            medico.nombre = "Nombre Prueba";
+            medico.apellido = "apellido prueba";
+            medico.identificacion = "10941234";
+            Assert.IsNotNull(controller.Create(medico));
+
+        }
+
+        [TestMethod]
+        public void TestMedicosDelete()
+        {
+            Assert.IsNotNull(controller.DeleteConfirmed(2));
+        }
+
+        [TestMethod]
+        public void TestMedicosFind()
+        {
+            Assert.AreNotEqual("System.Web.Mvc.HttpNotFoundResult", controller.Details(1).ToString());
+        }
+
+        [TestMethod]
+        public void TestMedicosEdit()
+        {
+
+            Medicos medico = new Medicos();
+            medico.nombre = "Nombre Prueba";
+            medico.apellido = "apellido prueba";
+            medico.identificacion = "10941234";
+            medico.idMedico = 1;
+            Assert.IsNotNull(controller.Edit(medico));
+
+        }
+    }
+}
